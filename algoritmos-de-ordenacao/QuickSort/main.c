5f6c515a4df4a2b920230498e0207f5eea13c8ac
paulocsa <paulocandiani04@gmail.com>
2024-04-18 22:23:11 -0300

    .

--- algoritmos-de-ordenacao/QuickSort/main.c
+++ algoritmos-de-ordenacao/QuickSort/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+int particiona_random(int* v, int inicio, int fim){
+    int pivo_indice = (rand() % (fim-inicio+1)+inicio+);
+
+    troca(&v, pivo_indice, fim);
+
+    return particiona_random(&v, inicio, fim)
+}
+
+
+
+void quick_sort(int* v, int inicio, int fim){
+
+    if(inicio <  fim){
+        int pivo = particiona_random(&v, inicio, fim);
+
+        //esquerda
+        quick_sort(&v, inicio, pivo-1);
+
+        //direita
+        quick_sort(&v, pivo+1, fim);
+
+    }
+
+}
+
+
+
 int main()
 {
-    printf("Hello world!\n");
+    int v[] - {4,3,6,7,9,10,5,8};
+    quick_sort(&v,0,7);
     return 0;
 }
